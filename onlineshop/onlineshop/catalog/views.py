d50930f8bce9f389928790b936258031059bd249
Denys Levshun <djoni1vincent@djoni1vincent.local>
2026-05-04 20:49:25 +0200

    fead: add basic setup for templates and static files

--- onlineshop/onlineshop/catalog/views.py
+++ onlineshop/onlineshop/catalog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+from .models import Category, Product
+
 # Create your views here.
 
 
-def home(request):
-    return "helloddd world"
+def index(request):
+    """View function for home page of site."""
+    num_products = Product.objects.all().count()
+    categories = Category.objects.all()
+
+    context = {
+        "num_products": num_products,
+        "categories": categories,
+    }
+
+    return render(request, "catalog/index.html", context=context)
